963aaa49d8ac3552aa43709dacbaf58dcd857876
Rola Alnabulsi <rola.nabulsi@gmail.com>
2023-01-24 10:25:12 +0200

    leave application

--- human_resourse/human_resourse/doctype/employee/employee.py
+++ human_resourse/human_resourse/doctype/employee/employee.py
@@ -6,13 +6,11 @@ from frappe.model.document import Document
 
 class Employee(Document):
     def validate(self):
-        return
         self.fullName_()
 
     def fullName_(self):
-        return
-    #     fName = self.emplyee_first_name
-    #     mName = self.emplyee_middle_name
-    #     lName = self.emplyee_last_name
-    #     fullName = fName + " " + mName + " " + lName
-    #     self.emplyee_full_name = fullName
+        fName = self.emplyee_first_name
+        mName = self.emplyee_middle_name
+        lName = self.emplyee_last_name
+        fullName = fName + " " + mName + " " + lName
+        self.emplyee_full_name = fullName
